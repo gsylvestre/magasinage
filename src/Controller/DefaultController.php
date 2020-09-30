e6435f37eb759892ae099037ccd056b5545d9aba
Guillaume <gsylvestre@gmail.com>
2020-09-30 15:16:19 +0200

    demo doctrine

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,7 +18,7 @@ class DefaultController extends AbstractController
      * Le nom de la route sert à générer les URLs vers cette page
      * @Route("/", name="home")
      */
-    public function home()
+    public function home(Request $request)
     {
         //un tableau de produits qu'on veut passer à Twig
         //normalement, ce serait un tableau d'objets provenant de la bdd
@@ -22,6 +26,11 @@ class DefaultController extends AbstractController
 
         //idem
         $categories = ["nature", "bouffe"];
+
+        //s'affichera dans la debug bar, sous le bouton ⌖
+        dump($categories);
+
+        //return $this->redirectToRoute('cgv');
 
         //on affiche le fichier twig (voir dans le répertoire templates/)
         return $this->render('default/home.html.twig', [
@@ -43,5 +52,96 @@ class DefaultController extends AbstractController
         //attention, on passe le nom des variables sous forme de chaîne !
         //certaines personnes préfèrent utiliser le compact pour éviter les répétitions
         return $this->render("default/cgv.html.twig", compact("users"));
+    }
+
+    /**
+     *
+     * Route qui afficherait le détail d'un produit
+     * L'id du produit a affiché est intégré à la fin de l'URL grâce au {id}
+     *
+     * @Route(
+     *     "/details/{id}",
+     *     methods={"GET"},
+     *     name="product_detail",
+     *     requirements={"id": "\d+"}
+     * )
+     */
+    public function productDetail($id)
+    {
+        //requête à la bdd pour aller ce produit
+        //en fonction de son id dans l'URL
+
+        die($id);
+    }
+
+    /**
+     * @Route("/test-select", name="test_select")
+     */
+    public function testSelect(EntityManagerInterface $entityManager, ProductRepository $productRepository)
+    {
+        //autre manière de récupérer le repository
+
+        //$productRepository = $this->getDoctrine()->getRepository(Product::class);
+
+        //récupère toutes les données de la table, mais sans limite ni tri possible
+        $products = $productRepository->findAll();
+
+        //mieux que le findAll car plus souple !
+        //findBy(where, order by, limit, offset)
+        $products = $productRepository->findBy([], ["price" => "DESC"], 2);
+
+        //récupère un product en fonction de sa clé primaire
+        //le résultat sera directement l'instance, sans tableau
+        $product = $productRepository->find(2);
+
+        //modifie le produit dans la bdd (change son prix)
+        $product->setPrice(6666);
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        //supprime le produit #2
+        //$entityManager->remove($product);
+        //$entityManager->flush();
+
+        dump($product);
+        dd($products);
+    }
+
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test(EntityManagerInterface $entityManager)
+    {
+        //crée une instance
+        $product = new Product();
+
+        //hydrate l'instance
+        $product->setName("Sirop d'érable");
+        $product->setPrice(4.99);
+        $product->setDateCreated(new \DateTime());
+
+
+         //puisque les setters sont fluides, on peut enchaîner les appels à chacun d'eux en une seule instruction
+        /*
+        $product
+            ->setName("pifpouf")
+            ->setPrice(5235)
+            ->setDateCreated(new \DateTime());
+        */
+
+        //j'aurais pu utiliser ça, mais je préfère utiliser les arguments de la méthode
+        //$entityManager = $this->getDoctrine()->getManager();
+
+        //demande à doctrine de sauvegarder mon instance
+        //attention, ça n'exécute pas tout de suite la requête
+        $entityManager->persist($product);
+
+        //exécute vraiment la requête sql maintenant
+        $entityManager->flush();
+
+        return $this->render('default/test.html.twig', [
+            'product' => $product
+        ]);
     }
 }
---
+++ src/Entity/Product.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="decimal", precision=6, scale=2)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateCreated;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+        return $this->dateCreated;
+    }
+
+    public function setDateCreated(\DateTimeInterface $dateCreated): self
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+}
